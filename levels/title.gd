39f69a7d0dda5bf1f611313c67805632d5afdf2a
bulbatf7-lgtm <bulbatf7@gmail.com>
2026-06-18 13:50:45 -0700

    hover states

--- levels/title.gd
+++ levels/title.gd
@@ -3,8 +3,13 @@ extends Node3D
 
 
 func _ready() -> void:
-	$TitleUI/Intro.play()
+	$TitleUI/Intro.queue_free()
+	#$TitleUI/Intro.play()
 
 
 func _on_intro_finished() -> void:
 	$TitleUI/Intro.queue_free()
+
+
+func _title_button_hovered() -> void:
+	$Hover.play()
