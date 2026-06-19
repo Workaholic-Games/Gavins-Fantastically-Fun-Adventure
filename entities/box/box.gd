8f62ea36032aec1ed7e92e21c1f222f92bb6c58a
NinjaBot2010 <jonathan.emmons@outlook.com>
2026-06-18 22:09:43 -0700

    KEYBINDS

--- entities/box/box.gd
+++ entities/box/box.gd
@@ -1,17 +1,14 @@
 extends StaticBody3D
 var played : bool = false
 
-
-
 func interact():
 	print("HELLO I AM A BOX")
-
-
 
 func outline():
 	if played == false:
 		$Outline.play("outline")
 		played = true
+		$InteractButton.text = InputMap.action_get_events("interact")[0].as_text()
 		$InteractButton.show()
 
 func no_outline():
